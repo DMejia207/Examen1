3f1986da83c39d6edce0b0c4f88054c4145efd40
DMejia207 <denisisaacmejiarodriguez@gmail.com>
2023-03-07 17:44:48 -0600

    Cambios el 7/3/2023 a las 5:46

--- Examen1/Views/PagePrincipal.xaml.cs
+++ Examen1/Views/PagePrincipal.xaml.cs
@@ -19,23 +19,40 @@ namespace Examen1.Views
 
         private async void btnguardar_Clicked(object sender, EventArgs e)
         {
-            var infor = new Models.Contactos
+            if (string.IsNullOrEmpty(txtnombre.Text))
             {
-                nombres = txtnombre.Text,
-                apellidos = txtapellidos.Text,
-                phone = txtphone.Text,
-                nota = txtnota.Text,
-                pais = cbpais.SelectedItem.ToString(),
-                edad = cbedad.SelectedItem.ToString()
-            };
+                await DisplayAlert("Alerta", "Ingrese un Nombre.", "OK");
+                txtnombre.Focus();
 
-          //  if (txtnombre.Text == " " && txtphone.Text == " " && txtnota.Text == " ")
-          //  {
-          //      await DisplayAlert("Alerta", "Llene los Campos Faltantes.", "OK");
-                
-          //  }
-           // else
-                      
+            }
+            else
+
+             if (string.IsNullOrEmpty(txtphone.Text))
+            {
+                await DisplayAlert("Alerta", "Ingrese un Numero de Telefono.", "OK");
+                txtphone.Focus();
+
+            }
+            else
+
+            if (string.IsNullOrEmpty(txtnota.Text))
+            {
+                await DisplayAlert("Alerta", "Ingrese una Nota.", "OK");
+                txtnota.Focus();
+
+            }
+            else
+            {
+                var infor = new Models.Contactos
+                {
+                    nombres = txtnombre.Text,
+                    apellidos = txtapellidos.Text,
+                    phone = txtphone.Text,
+                    nota = txtnota.Text,
+                    pais = cbpais.SelectedItem.ToString(),
+                    edad = cbedad.SelectedItem.ToString()
+                };
+
                 if (await App.DBlog.SaveListaa(infor) > 0)
                     await DisplayAlert("Alerta", "Usuario Ingresado con Exito", "OK");
                 else
@@ -45,8 +62,8 @@ namespace Examen1.Views
                 page.BindingContext = infor;
                 await Navigation.PushAsync(page);
 
-            return;
-            
+                
+            }
         }
       }
  }
